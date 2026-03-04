9846fff79c9a882a196a84684dde60dc9d0aa71e
dolf <rudolfbyker@gmail.com>
2026-03-04 16:35:20 +0200

    Add missing exports.

--- src/run_with_logger/__init__.py
+++ src/run_with_logger/__init__.py
@@ -1,7 +1,9 @@
-from ._run_with_logger import run_with_logger
-from ._run_with_logger__ssh import run_with_logger__ssh
+from ._run_with_logger import run_with_logger, run_with_logger__cm
+from ._run_with_logger__ssh import run_with_logger__ssh, run_with_logger__ssh__cm
 
 __all__ = [
     "run_with_logger",
     "run_with_logger__ssh",
+    "run_with_logger__cm",
+    "run_with_logger__ssh__cm",
 ]
